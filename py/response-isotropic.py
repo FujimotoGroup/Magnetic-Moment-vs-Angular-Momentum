9ab7e1baf0119df799abfd2a95595bea3fdd4530
jfujimoto <jf@titan>
2022-04-24 20:33:17 +0900

    pyでisotropicの場合を書き出した

--- py/response-isotropic.py
+++ py/response-isotropic.py
@@ -6,6 +6,7 @@ import matplotlib.animation as animation
 from scipy.interpolate import griddata
 import pandas as pd
 import configparser as cnf
+from matplotlib.ticker import ScalarFormatter
 
 home = "../"
 data = "../dat/"
@@ -29,94 +30,150 @@ EL0 = float(physics.get('EL0'))
 EL2 = float(physics.get('EL2'))
 delta = (EL2 - EL0)/2e0
 
+angstrom = 1e-10
+hbar     = 6.582119569e-16
+mass     = 9.10938356e-31
+charge   = 1.60217662e-19
+muB      = 9.2740100783e-24
+muBeV    = 5.7883818060e-5
+v0       = 1e6
+g_ast    = 2e0*mass*v0**2/(delta*charge)
+
 numeric = config['numeric']
 
+markers = ["o", ",", "D", "v", "^", "<", ">", "s", "p", "1", "2"]
 colors =['k', 'b', 'g', 'r', 'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan', 'navy', 'indigo']
+
+n = 1000.0
+mu_range1 = np.linspace((-delta-5e-2)*n, -delta*n, int(n))
+x1 = mu_range1 / n
+n = 100.0
+mu_range2 = np.linspace(-delta*n, 0e0, int(n))
+x2 = mu_range2 / n
+x = np.append(x1, x2)
+
+epsilon = 1e-4
+label = "{:.6f}".format(epsilon)
+str_damping = "Gamma = "+"{:.2f}".format(epsilon*1e3)+" [meV]"
 
 # dos {{{
 # 1,1 layout -  {{{
-def dos(x):
-    if abs(x - delta) > 0e0:
-        return np.abs(x)*np.sqrt(x**2 - delta**2)
-    else:
-        return 0e0
-
-fig, axes = plt.subplots(1,1,figsize=(10,10))
-i = 3
-label = "{:.6f}".format(1e-1**(i+1))
+fig, axes = plt.subplots(1,1,figsize=(7,7))
 readfile = data+'L1_'+str(bandsL)+'bands/dos_eps'+label+'.csv'
 dos = pd.read_csv(readfile,header=0).values
 
-n = 1000.0
-mu_range = np.linspace(dos[:,0].min()*n, dos[:,0].max()*n, 1000)
-mu_range = mu_range / n
+dos_analytic1 = np.abs(x1)*np.sqrt(x1**2 - delta**2) / (2e0*np.pi**2) / (hbar*v0)**3
+dos_analytic2 = np.full(int(n), 0e0)
+dos_analytic  = np.append(dos_analytic1, dos_analytic2)
 
+axes.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+axes.ticklabel_format(style="sci",  axis="y", scilimits=(0,0))
 axes.set_title("dos")
-axes.set_xlabel("mu")
-axes.set_ylim(-0.00001,0.001)
-axes.scatter(dos[:,0], dos[:,1], s=2, label="numeric")
+axes.set_xlabel("mu [eV]")
+axes.set_ylabel("DOS [/eV m3]")
+axes.scatter(dos[:,0], dos[:,1], s=4, label="numeric")
+axes.plot(x, dos_analytic, color=colors[0], label="analytic")
+axes.legend()
 
-#plt.savefig(png+"conductivity_T-"+str(bandsT)+"bands.png", bbox_inches = 'tight', dpi=300)
-#plt.rc("svg", fonttype="none")
-#plt.savefig(svg+"conductivity_T-"+str(bandsT)+"bands.svg")
-plt.show()
+plt.savefig(png+"dos_L-"+str(bandsL)+"bands-isotropic.png", bbox_inches = 'tight', dpi=300)
+plt.rc("svg", fonttype="none")
+plt.savefig(svg+"dos_L-"+str(bandsL)+"bands-isotropic.svg")
+#plt.show()
 plt.close()
 # }}}
 # }}}
 
 # L conductivity {{{
 # 1,2 layout - diagonal vs off-diagonal {{{
-fig, axes = plt.subplots(1,2,figsize=(10,5))
+fig, axes = plt.subplots(1,2,figsize=(14,7))
 axes = axes.flatten()
-i = 3
-label = "{:.6f}".format(1e-1**(i+1))
 readfile = data+'L1_'+str(bandsL)+'bands/conductivity_real_eps'+label+'.csv'
 conductivity = pd.read_csv(readfile,header=0).values
+window = [-1e-2*conductivity[:,1].max(), conductivity[:,1].max()*1.1e0]
+for ax in axes:
+    ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+    ax.ticklabel_format(style="sci",  axis="y", scilimits=(0,0))
 axes[0].set_title("diagonal")
-axes[0].set_xlabel("mu")
-axes[0].set_ylabel("conductivity")
-axes[0].set_ylim(-0.2,17.5)
-axes[0].scatter(conductivity[:,0], conductivity[:,1], color=colors[3], s=1, label="xx")
-axes[0].scatter(conductivity[:,0], conductivity[:,5], color=colors[2], s=1, label="yy")
-axes[0].scatter(conductivity[:,0], conductivity[:,9], color=colors[1], s=1, label="zz")
+axes[0].set_xlabel("mu [eV]")
+axes[0].set_ylabel("conductivity [/Ohm m]")
+axes[0].set_ylim(window)
+axes[0].scatter(conductivity[:,0], conductivity[:,1], s=3, marker=markers[0], edgecolors=colors[3], facecolor='None', label="xx")
+axes[0].scatter(conductivity[:,0], conductivity[:,5], s=3, marker=markers[1], edgecolors=colors[2], facecolor='None', label="yy")
+axes[0].scatter(conductivity[:,0], conductivity[:,9], s=3, marker=markers[3], edgecolors=colors[1], facecolor='None', label="zz")
 axes[0].legend()
 
 axes[1].set_title("off-diagonal")
-axes[1].set_xlabel("mu")
-axes[1].set_ylim(-0.2,17.5)
-axes[1].scatter(conductivity[:,0], conductivity[:,2], color=colors[3], s=1, label="xy")
-axes[1].scatter(conductivity[:,0], conductivity[:,3], color=colors[2], s=1, label="xz")
-axes[1].scatter(conductivity[:,0], conductivity[:,4], color=colors[1], s=1, label="yx")
-axes[1].scatter(conductivity[:,0], conductivity[:,6], color=colors[4], s=1, label="yz")
-axes[1].scatter(conductivity[:,0], conductivity[:,7], color=colors[5], s=1, label="zx")
-axes[1].scatter(conductivity[:,0], conductivity[:,8], color=colors[6], s=1, label="zy")
+axes[1].set_xlabel("mu [eV]")
+axes[1].set_ylim(window)
+axes[1].scatter(conductivity[:,0], conductivity[:,2], s=3, marker=markers[0], edgecolors=colors[3], facecolor='None', label="xy")
+axes[1].scatter(conductivity[:,0], conductivity[:,3], s=3, marker=markers[1], edgecolors=colors[2], facecolor='None', label="xz")
+axes[1].scatter(conductivity[:,0], conductivity[:,4], s=3, marker=markers[3], edgecolors=colors[1], facecolor='None', label="yx")
+axes[1].scatter(conductivity[:,0], conductivity[:,6], s=3, marker=markers[2], edgecolors=colors[4], facecolor='None', label="yz")
+axes[1].scatter(conductivity[:,0], conductivity[:,7], s=3, marker=markers[4], edgecolors=colors[5], facecolor='None', label="zx")
+axes[1].scatter(conductivity[:,0], conductivity[:,8], s=3, marker=markers[5], edgecolors=colors[6], facecolor='None', label="zy")
 axes[1].legend()
 
-axes[1].text(-0.02, 19, "eps = "+label+" [eV]")
+ax_pos = axes[1].get_position()
+fig.text(ax_pos.x1 - 0.15, ax_pos.y1 + 0.1, str_damping)
 
-plt.savefig(png+"conductivity_L1-"+str(bandsL)+"bands.png", bbox_inches = 'tight', dpi=300)
+plt.savefig(png+"conductivity_L-"+str(bandsL)+"bands-isotropic.png", bbox_inches = 'tight', dpi=300)
 plt.rc("svg", fonttype="none")
-plt.savefig(svg+"conductivity_L1-"+str(bandsL)+"bands.svg")
+plt.savefig(svg+"conductivity_L-"+str(bandsL)+"bands-isotropic.svg")
+#plt.show()
+plt.close()
+# }}}
+# 1,1 layout -  {{{
+sigma_analytic1 = (x1**2 - delta**2)/x1**2 / 3e0 * hbar / (2e0*epsilon) * (charge*v0)**2 * dos_analytic1 / charge
+sigma_analytic2 = np.full(int(n), 0e0)
+sigma_analytic  = np.append(sigma_analytic1, sigma_analytic2)
+
+fig, axes = plt.subplots(1,1,figsize=(7,7))
+
+axes.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+axes.ticklabel_format(style="sci",  axis="y", scilimits=(0,0))
+
+axes.set_title("compare")
+axes.set_xlabel("mu [eV]")
+axes.set_ylabel("conductivity [/Ohm m]")
+axes.set_ylim(window)
+axes.scatter(conductivity[:,0], conductivity[:,1], s=3, marker=markers[0], edgecolors=colors[3], facecolor='None', label="xx")
+axes.scatter(conductivity[:,0], conductivity[:,5], s=3, marker=markers[1], edgecolors=colors[2], facecolor='None', label="yy")
+axes.scatter(conductivity[:,0], conductivity[:,9], s=3, marker=markers[3], edgecolors=colors[1], facecolor='None', label="zz")
+axes.plot(x, sigma_analytic, label="analytic")
+axes.legend()
+
+ax_pos = axes.get_position()
+fig.text(ax_pos.x1 - 0.15, ax_pos.y1 + 0.1, str_damping)
+
+plt.savefig(png+"conductivity_L-"+str(bandsT)+"bands_compare-isotropic.png", bbox_inches = 'tight', dpi=300)
+plt.rc("svg", fonttype="none")
+plt.savefig(svg+"conductivity_L-"+str(bandsT)+"bands_compare-isotropic.svg")
 #plt.show()
 plt.close()
 # }}}
 # }}}
-# L spin conductivity {{{
+common_coef = - g_ast
+coef_analytic = - charge * v0**2 * hbar * common_coef
+coef_numeric = common_coef
+# L spin conductivity 1 {{{
 # 1,3 layout -  {{{
-#fig, axes = plt.subplots(1,3,figsize=(10,5))
-fig, axes = plt.subplots(1,3)
+fig, axes = plt.subplots(1,3,figsize=(21,7))
 axes = axes.flatten()
-i = 3
-label = "{:.6f}".format(1e-1**(i+1))
 readfile = data+'L1_'+str(bandsL)+'bands/spin_Hall_conductivity1_real_eps'+label+'.csv'
 df = pd.read_csv(readfile,header=0)
 titles = df.columns.values
 conductivity = df.values
-window = [-0.015,0.015]
+conductivity[:,1:-1] = conductivity[:,1:-1] * coef_numeric
+window = [-np.abs(conductivity[:,6].max()*1.1e0),
+           np.abs(conductivity[:,6].max()*1.1e0)]
+
+for ax in axes:
+    ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+    ax.ticklabel_format(style="sci",  axis="y", scilimits=(0,0))
 
 axes[0].set_ylabel("spin conductivity")
 axes[0].set_title("off-diagonal 1")
-axes[0].set_xlabel("mu")
+axes[0].set_xlabel("mu [eV]")
 axes[0].set_ylim(window)
 plot_lists = [6, 16, 20]
 for j in plot_lists:
@@ -124,7 +181,7 @@ for j in plot_lists:
 axes[0].legend()
 
 axes[1].set_title("off-diagonal 2")
-axes[1].set_xlabel("mu")
+axes[1].set_xlabel("mu [eV]")
 axes[1].set_ylim(window)
 plot_lists = [8, 12, 22]
 for j in plot_lists:
@@ -132,49 +189,138 @@ for j in plot_lists:
 axes[1].legend()
 
 axes[2].set_title("others")
-axes[2].set_xlabel("mu")
+axes[2].set_xlabel("mu [eV]")
 axes[2].set_ylim(window)
 plot_lists = [1, 2, 3, 4, 5, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19, 21, 23, 24, 25, 26, 27]
 for j in plot_lists:
     axes[2].scatter(conductivity[:,0], conductivity[:,j], s=1, label=titles[j])
 axes[2].legend()
 
-axes[2].text(-0.02, 19, "eps = "+label+" [eV]")
+ax_pos = axes[2].get_position()
+fig.text(ax_pos.x1 - 0.15, ax_pos.y1 + 0.05, str_damping)
 
-#plt.savefig(png+"spin_conductivity_L1-"+str(bandsL)+"bands.png", bbox_inches = 'tight')
-plt.savefig(png+"spin_conductivity_L1-"+str(bandsL)+"bands.png")
+plt.savefig(png+"spin_conductivity1_L-"+str(bandsL)+"bands-isotropic.png", bbox_inches = 'tight', dpi=300)
 plt.rc("svg", fonttype="none")
-plt.savefig(svg+"spin_conductivity_L1-"+str(bandsL)+"bands.svg")
+plt.savefig(svg+"spin_conductivity1_L-"+str(bandsL)+"bands-isotropic.svg")
 #plt.show()
 plt.close()
 # }}}
 # 1,1 layout -  {{{
-fig, axes = plt.subplots(1,1,figsize=(10,10))
-i = 3
-label = "{:.6f}".format(1e-1**(i+1))
+sigma_analytic1 = np.sqrt(x1**2 - delta**2) / np.abs(x1) / (2e0*np.pi**2) / (hbar*v0)**3 * delta * coef_analytic
+sigma_analytic2 = np.full(int(n),0e0)
+sigma_analytic  = np.append(sigma_analytic1, sigma_analytic2)
+
+fig, axes = plt.subplots(1,1,figsize=(7,7))
 readfile = data+'L1_'+str(bandsL)+'bands/spin_Hall_conductivity1_real_eps'+label+'.csv'
 df = pd.read_csv(readfile,header=0)
 titles = df.columns.values
 conductivity = df.values
+conductivity[:,1:-1] = conductivity[:,1:-1] * coef_numeric
 
-n = 1000.0
-mu_range = np.linspace(conductivity[:,0].min()*n, conductivity[:,0].max()*n, 1000)
-mu_range = mu_range / n
+axes.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+axes.ticklabel_format(style="sci",  axis="y", scilimits=(0,0))
 
-axes.set_title("fitting")
-axes.set_xlabel("mu")
-axes.set_ylim(-0.001,0.015)
+axes.set_title("compare")
+axes.set_xlabel("mu [eV]")
+axes.set_ylabel("SHC1 [/Ohm m]")
+#axes.set_ylim(-0.001,0.015)
 plot_lists = [6, 16, 20]
 for j in plot_lists:
     axes.scatter(conductivity[:,0], conductivity[:,j], s=2, label=titles[j])
+axes.plot(x, sigma_analytic, label="analytic")
 axes.legend()
 
-axes.text(-0.02, 19, "eps = "+label+" [eV]")
+ax_pos = axes.get_position()
+fig.text(ax_pos.x1 - 0.15, ax_pos.y1 + 0.1, str_damping)
 
-#plt.savefig(png+"conductivity_T-"+str(bandsT)+"bands.png", bbox_inches = 'tight', dpi=300)
-#plt.rc("svg", fonttype="none")
-#plt.savefig(svg+"conductivity_T-"+str(bandsT)+"bands.svg")
-plt.show()
+plt.savefig(png+"spin_conductivity1_L-"+str(bandsL)+"bands_compare-isotropic.png", bbox_inches = 'tight', dpi=300)
+plt.rc("svg", fonttype="none")
+plt.savefig(svg+"spin_conductivity1_L-"+str(bandsL)+"bands_compare-isotropic.svg")
+#plt.show()
+plt.close()
+# }}}
+# }}}
+# L spin conductivity 2 {{{
+# 1,3 layout -  {{{
+fig, axes = plt.subplots(1,3,figsize=(21,7))
+axes = axes.flatten()
+readfile = data+'L1_'+str(bandsL)+'bands/spin_Hall_conductivity2_real_eps'+label+'.csv'
+df = pd.read_csv(readfile,header=0)
+titles = df.columns.values
+conductivity = df.values
+conductivity[:,1:-1] = conductivity[:,1:-1] * coef_numeric
+
+for ax in axes:
+    ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+    ax.ticklabel_format(style="sci",  axis="y", scilimits=(0,0))
+
+axes[0].set_ylabel("spin conductivity [/Ohm m]")
+axes[0].set_title("off-diagonal 1")
+axes[0].set_xlabel("mu [eV]")
+axes[0].set_ylim(window)
+plot_lists = [6, 16, 20]
+for j in plot_lists:
+    axes[0].scatter(conductivity[:,0], conductivity[:,j], s=1, label=titles[j])
+axes[0].legend()
+
+axes[1].set_title("off-diagonal 2")
+axes[1].set_xlabel("mu [eV]")
+axes[1].set_ylim(window)
+plot_lists = [8, 12, 22]
+for j in plot_lists:
+    axes[1].scatter(conductivity[:,0], conductivity[:,j], s=1, label=titles[j])
+axes[1].legend()
+
+axes[2].set_title("others")
+axes[2].set_xlabel("mu [eV]")
+axes[2].set_ylim(window)
+plot_lists = [1, 2, 3, 4, 5, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19, 21, 23, 24, 25, 26, 27]
+for j in plot_lists:
+    axes[2].scatter(conductivity[:,0], conductivity[:,j], s=1, label=titles[j])
+axes[2].legend()
+
+ax_pos = axes[2].get_position()
+fig.text(ax_pos.x1 - 0.15, ax_pos.y1 + 0.05, str_damping)
+
+plt.savefig(png+"spin_conductivity2_L-"+str(bandsL)+"bands-isotropic.png", bbox_inches = 'tight', dpi=300)
+plt.rc("svg", fonttype="none")
+plt.savefig(svg+"spin_conductivity2_L-"+str(bandsL)+"bands-isotropic.svg")
+#plt.show()
+plt.close()
+# }}}
+# 1,1 layout -  {{{
+cutoff = np.abs(- delta - 5e-2)
+sigma_analytic1 = np.log(2*cutoff / (np.abs(x1) + np.sqrt(x1**2 - delta**2))) / (2e0*np.pi**2) / (hbar*v0)**3 * delta
+sigma_analytic2 = np.full(int(n),np.log(2e0*cutoff/np.abs(delta))) / (2e0*np.pi**2) / (hbar*v0)**3 * delta
+sigma_analytic  = np.append(sigma_analytic1, sigma_analytic2)
+sigma_analytic = sigma_analytic * coef_analytic
+fig, axes = plt.subplots(1,1,figsize=(7,7))
+readfile = data+'L1_'+str(bandsL)+'bands/spin_Hall_conductivity2_real_eps'+label+'.csv'
+df = pd.read_csv(readfile,header=0)
+titles = df.columns.values
+conductivity = df.values
+conductivity[:,1:-1] = conductivity[:,1:-1] * coef_numeric
+
+axes.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+axes.ticklabel_format(style="sci",  axis="y", scilimits=(0,0))
+
+axes.set_title("compare")
+axes.set_xlabel("mu [eV]")
+axes.set_ylabel("SHC2 [/Ohm m]")
+#axes.set_ylim(-0.001,0.015)
+plot_lists = [6, 16, 20]
+for j in plot_lists:
+    axes.scatter(conductivity[:,0], conductivity[:,j], s=2, label=titles[j])
+axes.plot(x, sigma_analytic, label="analytic")
+axes.legend()
+
+ax_pos = axes.get_position()
+fig.text(ax_pos.x1 - 0.15, ax_pos.y1 + 0.1, str_damping)
+
+plt.savefig(png+"spin_conductivity2_L-"+str(bandsL)+"bands_compare-isotropic.png", bbox_inches = 'tight', dpi=300)
+plt.rc("svg", fonttype="none")
+plt.savefig(svg+"spin_conductivity2_L-"+str(bandsL)+"bands_compare-isotropic.svg")
+#plt.show()
 plt.close()
 # }}}
 # }}}
