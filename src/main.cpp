19cdfef0d4a3188f168379c3505703f93fc4c390
Fujimoto Junji <fujimoto.junji+ts@gmail.com>
2022-05-06 09:11:42 +0900

    点が荒すぎた

--- src/main.cpp
+++ src/main.cpp
@@ -233,7 +233,7 @@ int main(){
 //    chemical_potential mu = 0e0;
 ////    for(int valley=0; valley<valleys; valley++) {
         mu_cutoff_L = 1e-1;
-        mu_cutoff_mesh_L = 50;
+        mu_cutoff_mesh_L = 20;
 //        chemical_potential mu_max = double(EL[valley][band_index])+4e-1;
         chemical_potential mu_min = double(EL[valley][band_index])-1e-2;
         chemical_potential mu_max = 8e-2;
@@ -252,7 +252,7 @@ int main(){
     auto calc_h = std::chrono::duration_cast<std::chrono::hours>(dur);
     auto dur_m = dur - std::chrono::duration_cast<std::chrono::seconds>(calc_h);
     auto calc_m = std::chrono::duration_cast<std::chrono::minutes>(dur_m);
-    auto dur_s = dur_m - std::chrono::duration_cast<std::chrono::seconds>(dur_m);
+    auto dur_s = dur_m - std::chrono::duration_cast<std::chrono::seconds>(calc_m);
     auto calc_s = std::chrono::duration_cast<std::chrono::seconds>(dur_s);
     std::cout << "calc duration: " << calc_h.count() << " h " << calc_m.count() << " m " << calc_s.count() << " s." << std::endl;
     return 0;
